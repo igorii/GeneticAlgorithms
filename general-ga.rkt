d56f492702692f4b48c3d6d4891a23e75d7c3135
timthornton <t.thornton6@gmail.com>
2014-09-26 15:55:34 -0400

    Add each solution file

---
+++ general-ga.rkt
@@ -0,0 +1,192 @@
+#lang racket
+
+(provide run-ga)
+
+;; Data structures
+;; PopulationTable
+;;    individual -> fitness
+;;    (hash-keys h) ; gets the keys as a list
+;;    (hash-values h)
+;;    (hash->list h) ; key value pairs
+
+;; UTIL
+(define (zip l1 l2) (map cons l1 l2))
+(define (chance percent-true) (>= percent-true (random)))
+(define (print-with-cross-point chromosome cross-point)
+  (define (f x curr acc)
+    (if (null? x) 
+      (begin (display acc) (newline))
+      (if (= curr cross-point)
+        (begin
+          (let ([final (append acc (list #\_) x)])
+            (display final) (newline)))
+        (f (cdr x) (add1 curr) (append acc (list (car x)))))))
+  (f chromosome 0 '()))
+;; END UTIL
+
+; Initialize one random individual
+(define (create-one-random-binary strlen)
+  (map (λ (x) (if (chance 0.5) 1 0))
+       (range 0 strlen)))
+
+;(: initialize-population (Number Number -> (Listof Chromosome)))
+;; Initialize a random population of `popsize` many
+;; individuals of length `strlen`
+(define (initialize create-fn strlen popsize)
+
+  ; Initialize all individuals
+  (define (f n acc-dict)
+    (if (= n 0)
+      (map car (hash->list acc-dict))
+      (let ([candidate (create-fn strlen)])
+        (if (hash-has-key? acc-dict candidate)
+          ; To ensure diversity in the initial population, skip any duplicates
+          (f n acc-dict)
+          (f (- n 1) (hash-set acc-dict candidate null))))))
+  (f popsize (make-immutable-hash '())))
+
+;(: fitness (Chromosome -> Number))
+(define (total-fitness fs)   (foldl + 0 fs))
+
+;(: assess-all ((Listof Chromosome) -> (Listof Number)))
+;; Assess the fitness of every individual
+;(define assess-all map)
+
+;(: mutate (Real Chromosome -> Chromosome))
+;; Apply a random mutation to an individual
+(define (binary-mutate-fn p individual) 
+  (define (swap x) (if (= x 0) 1 0))
+  (map (λ (x) (if (chance p) (swap x) x)) individual))
+
+;(: recombine (Chromosome Chromosome Number -> (Listof Chromosome)))
+;; Apply recombinational crossover to two parents to
+;; produce two children
+(define (crossover-one-point parent1 parent2 cross-point)
+  (define (get-child op cross-point p1 p2)
+    (define (f p1 p2 curr acc)
+      (if (or (null? p1) (null? p2))
+        acc
+        (let ([next (if (op curr cross-point) (car p1) (car p2))])
+          (f (cdr p1) (cdr p2) (add1 curr) (append acc (list next))))))
+    (f parent1 parent2 0 '()))
+
+  ;; Create the two children
+  (list (get-child <= cross-point parent1 parent2)
+        (get-child >  cross-point parent1 parent2)))
+
+;; from slide 19
+(define (selection-roulette pop fs)
+  (define (f wp partial-sum pop fs)
+    (let ([next-fitness (+ partial-sum (car fs))])
+      (cond [(null? (cdr fs))    (car pop)]
+            [(> next-fitness wp) (car pop)]
+            [else                (f wp next-fitness (cdr pop) (cdr fs))])))
+  (f (* (random) (total-fitness fs)) 0 pop fs))
+
+(define (selection-tournament size pop fs) 
+  (let* ([fs.pop       (zip fs pop)]
+         [participants (take (shuffle fs.pop) size)]
+         [winners      (take (sort participants (λ (f.p1 f.p2) (< (car f.p1) (car f.p2)))) 2)])
+    winners))
+
+; Generic GA
+; ==========
+
+;; TODO  - Add elitism flag
+(define (run-ga #:string-length     strlen
+                #:population-size   popsize
+                #:minimum-fitness   [minfit 0]
+                #:cutoff            [cutoff 1]
+                #:create-individual [create-fn create-one-random-binary]
+                #:use-elitism       [use-elitism #f]
+                #:mutate-fn         [mutate-fn binary-mutate-fn]
+                #:mutation-prob     [mutate-prob (exact->inexact (/ 1 strlen))]
+                #:fitness-eval      fitness-fn)
+
+  (define (create-next-gen last-gen fits popsize strlen)
+    (define (f i acc)
+      (if (= i 0)
+        acc
+        ;   Pick p1 with replacement
+        (let* ([parent1     (selection-roulette last-gen fits)]
+               ;   Pick p2 with replacement
+               [parent2     (selection-roulette last-gen fits)]
+               ;   Create children c1, c2 from parents
+               [cross-point (random strlen)]
+               [children    (crossover-one-point parent1 parent2 cross-point)]
+               ;   Mutate both children
+               [mutated-c1  (mutate-fn mutate-prob (car  children))]
+               [mutated-c2  (mutate-fn mutate-prob (cadr children))])
+          ;   Add mutated children to next population
+          (f (- i 1) (append acc (list mutated-c1 mutated-c2))))))
+
+    ; Initialize the next population as an empty list
+    ; For half the population size, do
+    (f (floor (/ popsize 2)) '()))
+
+  (define (elitism best pop)
+    (cons best (cdr (shuffle pop))))
+
+  ; repeat
+  (define (loop pop last-best)
+    ; Assess each individual
+    (let* ([fits      (map fitness-fn pop)]
+           [zipfits   (zip fits pop)]
+           ;   Update best
+           [curr-best (argmax car zipfits)])
+
+      (begin (display curr-best) (newline)
+             ;   If best is good enough, return it
+             (if (>= (car curr-best) cutoff)
+               curr-best  ;; returns a pair of (fitness . individual)
+               (if use-elitism
+                 (loop (elitism (cdr curr-best) (create-next-gen pop fits popsize strlen)) curr-best)
+                 (loop (create-next-gen pop fits popsize strlen) curr-best))))))
+
+  ; Initialize population of individuals
+  (let ([init-pop (initialize create-fn strlen popsize)]
+        ; Initialize best to min value
+        [init-best (cons minfit null)])
+    (loop init-pop init-best)))
+
+
+
+;; TEST
+;(initialize 50 10)
+
+;(define (GA strlen popsize minfit cutoff fitness-fn)
+;(GA #:string-length   50
+;    #:population-size 100
+;    #:cutoff          50
+;    #:minimum-fitness 0
+;    #:fitness-eval    fitness)
+;;
+;(define population (initialize *string-length* *population-size*))
+;(define fs (assess-all fitness population))
+;(display population) (newline)
+;(define parent1 (selection-roulette population fs))
+;(define parent2 (selection-roulette population fs))
+;;
+;fs
+;(fitness parent1)
+;(fitness parent2)
+
+;;(map fitness (initialize *string-length* *population-size*))
+;(define cp (random-under *string-length*))
+;(display "Crossover point ") (display cp) (newline)
+;
+;(define parents (initialize *string-length* 2))
+;(display "Parent 1 ")
+;(print-with-cross-point (car parents) cp)
+;(display "Parent 2 ")
+;(print-with-cross-point (cadr parents) cp)
+;
+;(define children (recombine (car parents) (cadr parents) cp))
+;(display "Child  1 ")
+;(print-with-cross-point (car children) cp)
+;(display "Child  2 ")
+;(print-with-cross-point (cadr children) cp)
+;(display "Mutation ")
+;(print-with-cross-point (mutate 0.1 (cadr children)) cp)
+;
+;(assess-all fitness (append parents children))
---
+++ simple-max.rkt
@@ -0,0 +1,27 @@
+#lang racket
+
+(require "general-ga.rkt")
+
+(define (simple-max-fitness strlen)
+  (let ([half-mark (/ strlen 2)])
+    (λ (individual)
+       (let* ([numer (foldl * 1 (take individual half-mark))]
+              [denom (foldl * 1 (take (reverse individual) half-mark))])
+           (/ numer denom)))))
+
+(define (create-individual strlen)
+  (map (λ (x) (+ 1 (random 10)))
+       (range 0 strlen)))
+
+(define (mutate-individual p individual)
+  (define (m x) (+ 1 (random 10)))
+  (map (λ (x) (if (>= p (random)) (m x) x))
+       individual))
+
+(run-ga #:string-length     10
+        #:population-size   80
+        #:create-individual create-individual
+        #:mutate-fn         mutate-individual
+        #:cutoff            ((simple-max-fitness 10) (list 10 10 10 10 10 1 1 1 1 1))
+        #:use-elitism       #t
+        #:fitness-eval      (simple-max-fitness 10))
