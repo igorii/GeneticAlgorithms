4d933df23ae0379738aa90eee6364c944fe0f9c9
timthornton <t.thornton6@gmail.com>
2014-10-19 23:21:13 -0400

    Cleanup

--- tsp.rkt
+++ tsp.rkt
@@ -30,7 +30,7 @@
 ;; *************
 
 ;; Whether to start a GA run, or print sample output of crossovers and mutation
-(define *verbose* #t)
+(define *verbose* #f)
 
 ;; Whether the GA thread is paused or not
 (define *pause* #f)
@@ -43,6 +43,7 @@
 
 ;; Coordinate system identifiers
 (define *coords* (get-coords-from-file (args-file cargs) (args-sep cargs) (args-col cargs)))
+;(define *coords* (map (lambda (x) (list (* 1000 (random)) (* 1000 (random)))) (range 0 30)))
 
 ;; *****
 ;; Utils
@@ -115,14 +116,16 @@
   (let* ([post-p1 (get-from-p1 p1 '() #hash())]
          [unused  (get-unused p2 (cadr post-p1))]
          [child   (fill-nulls unused (car post-p1) '())])
+
     (if *verbose* 
       (begin
-        (display "Position Based Crossover")      (newline)
-        (display "    Parent 1: ") (display (convert-to-nums p1))       (newline)
-        (display "    Parent 2: ") (display (convert-to-nums p2))       (newline)
-        (display "    From p1 : ") (display (map (lambda (x) (if (null? x) #\_ x)) (convert-to-nums (car post-p1))))  (newline)
-        (display "    Child   : ") (display (convert-to-nums child))    (newline))
+        (displayln "Position Based Crossover")
+        (display "    Parent 1: ") (displayln (convert-to-nums p1))
+        (display "    Parent 2: ") (displayln (convert-to-nums p2))
+        (display "    From p1 : ") (displayln (map (lambda (x) (if (null? x) #\_ x)) (convert-to-nums (car post-p1))))
+        (display "    Child   : ") (displayln (convert-to-nums child)))
       null)
+
     child))
 
 (define (crossover-injection p1 p2) null)
@@ -159,42 +162,49 @@
   (let* ([points       (get-range strlen)]
          [post-phase1  (phase1 p1 p2 (car points) (cadr points) '() #hash())]
          [post-phase2  (phase2 p1 p2 (car points) (cadr points) (car post-phase1) (cadr post-phase1))]
-         [post-phase3  (fill-nulls (get-unused p2 (cadr post-phase2)) (car post-phase2) '())])
+         [child        (fill-nulls (get-unused p2 (cadr post-phase2)) (car post-phase2) '())])
+
     (if *verbose* 
       (begin
-        (display "Partially Mapped Crossover")                     (newline)
-        (display "    Parent 1: ") (display (add-points-at-indicies (convert-to-nums p1) (list #\| #\|) points))  (newline)
-        (display "    Parent 2: ") (display (add-points-at-indicies (convert-to-nums p2) (list #\| #\|) points))  (newline)
-        (display "    Points  : ") (display points)                (newline)
-        (display "    Part 1  : ") (display (add-points-at-indicies (map (lambda (x) (if (null? x) #\_ x)) (convert-to-nums (car post-phase1))) (list #\| #\|) points))     (newline)
-        (display "    Part 2  : ") (display (add-points-at-indicies (map (lambda (x) (if (null? x) #\_ x)) (convert-to-nums (car post-phase2))) (list #\| #\|) points))    (newline)
-        (display "    Child   : ") (display (add-points-at-indicies (convert-to-nums post-phase3) (list #\| #\|) points)) (newline))
+        (displayln "Partially Mapped Crossover")
+        (display "    Parent 1: ") (displayln (add-points-at-indicies (convert-to-nums p1) (list #\| #\|) points))
+        (display "    Parent 2: ") (displayln (add-points-at-indicies (convert-to-nums p2) (list #\| #\|) points))
+        (display "    Points  : ") (displayln points)
+        (display "    Part 1  : ") (displayln (add-points-at-indicies (map (lambda (x) (if (null? x) #\_ x)) (convert-to-nums (car post-phase1))) (list #\| #\|) points))
+        (display "    Part 2  : ") (displayln (add-points-at-indicies (map (lambda (x) (if (null? x) #\_ x)) (convert-to-nums (car post-phase2))) (list #\| #\|) points))
+        (display "    Child   : ") (displayln (add-points-at-indicies (convert-to-nums child) (list #\| #\|) points)))
       null)
-    post-phase3))
 
+    child))
 
 ;; **********
 ;; Selections
 ;; **********
 
+;; Return the selection function with the given name
 (define (lbl->selection lbl)
   (cond [(eq? lbl "Random")     (random-selection)]
         [(eq? lbl "Tournament") selection-tournament]
         [(eq? lbl "Roulette")   selection-roulette]
         [(eq? lbl "Ranked")     selection-ranked]))
 
+;; Return a random selection function
 (define (random-selection)
   (let* ([ss (vector selection-tournament selection-ranked)]
          [r  (random (vector-length ss))])
     (vector-ref ss r)))
 
+;; Perform a tournament selection on the population. Ignore the last param (ranked)
+;; since it is unused here.
 (define (selection-tournament fpop select-settings _)
-  ;(let* ([tpop (take fpop (s-select-tsize select-settings))])   ; Select the individuals for tournament
   (let* ([tpop (take (shuffle fpop) (s-select-tsize select-settings))])   ; Select the individuals for tournament
     (if (chance (s-select-bprob select-settings))
       (first (sort tpop (lambda (x y) (< (car x) (car y)))))              ; bprob % of the time take the best
       (first (shuffle tpop)))))                                           ; Otherwise take a random one
 
+;; Rank the population based on a scheme presented in the course notes. This
+;; avoids "super-individuals" since selection is based on reletive rank rather
+;; than absolute fitness.
 (define (rank-pop select-settings fpop popsize)
   (define base (s-select-ranked-base select-settings))
   (define u    (s-select-popsize     select-settings))
@@ -227,13 +237,19 @@
 
   ;; TODO - move sorting to main loop before selection (so it done only once per generation)
   (let* ([sorted (sort fpop (lambda (a b) (< (car a) (car b))))]  ; Sort the population
-         ;; Reverse the probabilities since we are minimizng the scores
-         [probs  (map assign (reverse (range 0 u)))]       ; Assign rank based probabilities
-         [sprobs (scan + 0 probs)])                        ; Scan addition over the probabilities
+         [probs  (map assign (reverse (range 0 u)))]              ; Assign rank based probabilities
+                                                                  ; Reverse the probabilities since we are minimizng the scores
+         [sprobs (scan + 0 probs)])                               ; Scan addition over the probabilities
     (if *verbose* (printf "\n     Probs: ~a\n     Total: ~a\n" probs (car (reverse sprobs))) null)
     (list sorted sprobs)))
 
+;; Perform ranked selection on a population. Rank probabilities have already
+;; been assigned at this point and are provided as the third param. This is 
+;; done ahead of time to avoid computing the ranks *per selection*, since the
+;; ranks will not change in a single generation.
 (define (selection-ranked fpop select-settings ranked)
+  ;; Linear search the population while the generated number is 
+  ;; greater than the current assigned probability
   (define (select r probs fpop)
     (if (< r (car probs))
       (car fpop)
@@ -244,8 +260,10 @@
     (if *verbose* (printf "     R: ~a\n" r) null)
     selected))
 
+;; Determine the total fitness of the population
 (define (total-fitness fs) (foldl + 0 fs))
 
+;; Perform a fitness-proportional selection on the population
 ;; from slide 19
 (define (selection-roulette fpop select-settings _)
   (define (loop wp partial-sum fpop)
@@ -343,6 +361,8 @@
 
   ;; Convenience for passing to selection functions
   (define select-settings (s-select tsize bprob ranked-base popsize))
+
+  ;; Store the length for convenience
   (define strlen (length domain))
 
   ;; Select two individuals from the given population with replacement and return
@@ -367,6 +387,8 @@
       (map (lambda (_) (tsp-crossover fpop ranked))
            (range 0 popsize))))
 
+  ;; Elitism ensures that the best genetic material is not lost when swapping
+  ;; generations. Add the current best to the next generation.
   (define (elitism pop best)
     (cons best (cdr pop)))
 
@@ -374,15 +396,20 @@
   (define (loop i oldpop strlen w)
     (if *pause*
       (loop i oldpop strlen w)
-      (let* ([fits  (map calc-fitness oldpop)]
-             [fpop  (zip fits oldpop)]
-             [best  (argmin car fpop)]
-             [worst (argmax car fpop)])
-        (cond [(<= (car best) cutoff)                     (callback i best worst w) best]
-              [(and (not (eq? max-gen 0)) (>= i max-gen)) (callback i best worst w) best]
-              [else (callback i best worst w)
-                    (loop (add1 i) (elitism (create-new-pop fpop) (cdr best)) strlen w)]))))
+      (let* ([fits    (map calc-fitness oldpop)]
+             [fpop    (zip fits oldpop)]
+             [best    (argmin car fpop)]
+             [worst   (argmax car fpop)])
+        (cond [(<= (car best) cutoff)
+               (callback i best worst w) best]
+              [(and (not (eq? max-gen 0)) (>= i max-gen))
+               (callback i best worst w) best]
+              [else 
+                (callback i best worst w)
+                (loop (add1 i) (elitism (create-new-pop fpop) (cdr best)) strlen w)]))))
 
+  ;; Initialize the population and set up the canvas dimensions
+  ;; Then kick off the GA
   (let* ([population (initialize-population (create-random-tour domain) 0 popsize)]
          [xmin   (car  (argmin car  (car population)))]
          [xmax   (car  (argmax car  (car population)))]
@@ -391,7 +418,6 @@
          [strlen (length (car population))])
     (set! *pause* #f)
     (loop 0 population strlen (world null xmin xmax ymin ymax))))
-
 
 ;; ***
 ;; GUI
@@ -462,10 +488,13 @@
                              [init-value "50"]
                              [parent middle-row-panel]))
 
+;; Start a GA in the GA thread, leaving the GUI thread open to receive and handle GUI events
 (define (new-run-thread)
   (if (thread? *ga-thread*) (kill-thread *ga-thread*) null)
   (set! *ga-thread* (thread new-run)))
 
+;; Initiate a new GA run n the current thread. The GA is initialized with the settings
+;; appearing in the GUI. The GA will loop until the cutoff is observed.
 (define (new-run)
   (let* ([cutoff (string->number (send best-score-field get-value))]
          [best (run-ga2 #:population-size (string->number (send pop-size-field get-value))
@@ -498,15 +527,26 @@
   (let* ([p1 ((create-random-tour *coords*) null)]
          [p2 ((create-random-tour *coords*) null)]
          [pop (initialize-population (create-random-tour *coords*) 0 10)])
+    (newline)
     (crossover-position-based   p1 p2 (length p1)) (newline)
     (crossover-partially-mapped p1 p2 (length p1)) (newline)
     (display "Mutation Inversion  ") (display (mutation-inversion (list 1 2 3 4 5 6 7 8 9 10) 10)) (newline)
     (display "Mutation Scramble   ") (display (mutation-scramble  (list 1 2 3 4 5 6 7 8 9 10) 10)) (newline)
     (display "Mutation Exchange   ") (display (mutation-exchange  (list 1 2 3 4 5 6 7 8 9 10) 10)) (newline)
     (display "Mutation Insertion  ") (display (mutation-insertion (list 1 2 3 4 5 6 7 8 9 10) 10)) (newline)
-    (display "Ranked Selection")
+
+    ;; Ranked Selection examples from notes
+    (newline)
+    (display "Ranked Selection (s=1.5)")
     (let* ([fpop     (list (list 1 1 1) (list 4 4 4) (list 5 5 5))]
            [settings (s-select 0 0 1.5 3)]
+           [ranked   (rank-pop settings fpop 3)])
+      (printf "     Result: ~a\n" (selection-ranked fpop settings ranked)))
+
+    (newline)
+    (display "Ranked Selection (s=2)")
+    (let* ([fpop     (list (list 1 1 1) (list 4 4 4) (list 5 5 5))]
+           [settings (s-select 0 0 2 3)]
            [ranked   (rank-pop settings fpop 3)])
       (printf "     Result: ~a\n" (selection-ranked fpop settings ranked)))
 
